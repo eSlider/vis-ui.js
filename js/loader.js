969a9ff659dc8bb0e3d2275f7a69a52fda8df41e
Andriy Oblivantsev <eslider@gmail.com>
2015-05-03 16:25:02 +0200

    add load popupDialog

--- js/loader.js
+++ js/loader.js
@@ -2,32 +2,38 @@
  *
  * @author Andriy Oblivantsev <eslider@gmail.com>
  */
-(function($) {
-    var requieredFiles = [
-        'js/utils/fn.formData.js',
-        'js/elements/data.result-table.js',
-        'js/elements/date.selector.js',
-        'js/elements/geo.toolset.js',
-        'js/elements/popup.dialog.js',
-        'js/elements/tab.navigator.js',
-        'js/jquery.form.generator.js'
-    ];
+var requieredFiles = [
+    'js/utils/fn.formData.js',
+    'js/elements/data.result-table.js',
+    'js/elements/date.selector.js',
+    'js/elements/geo.toolset.js',
+    'js/elements/popup.dialog.js',
+    'js/elements/tab.navigator.js',
+    'js/jquery.form.generator.js'
+];
 
-    var loadedFiles = 0;
+var loadedFiles = 0;
+var onComplete = null;
 
-    function checkLoad() {
-        loadedFiles++;
-        if(requieredFiles.length == loadedFiles) {
-            console.log("all loaded");
+function checkLoad() {
+    loadedFiles++;
+    if(requieredFiles.length == loadedFiles) {
+        if(onComplete){
+            onComplete();
         }
     }
+}
 
-    function onLoadError(e) {
-        console.log("Something goes wrong by load VI UI", this);
+function onLoadError(e) {
+    console.log("Something goes wrong by load VI UI", this);
+}
+
+function loadElements(completeHandler) {
+    if(completeHandler){
+        onComplete  = completeHandler;
     }
-
     $.each(requieredFiles, function(i, fileName) {
-        jQuery.getScript(fileName, checkLoad).error(onLoadError);
+        jQuery.getScript(fileName + "?ver=" + Math.random(), checkLoad).error(onLoadError);
     })
+}
 
-})(jQuery);
