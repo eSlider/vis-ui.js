9552fbbcc005c404cd9be089ab0ff0ad0227210a
Andriy Oblivantsev <eslider@gmail.com>
2017-12-13 18:00:57 +0100

    Add and style copy clipboard option

--- src/js/jquery.form.generator.js
+++ src/js/jquery.form.generator.js
@@ -90,6 +90,35 @@
         });
     }
 
+    // Copies a string to the clipboard. Must be called from within an
+    // event handler such as click. May return false if it failed, but
+    // this is not always possible. Browser support for Chrome 43+,
+    // Firefox 42+, Safari 10+, Edge and IE 10+.
+    // IE: The clipboard feature may be disabled by an administrator. By
+    // default a prompt is shown the first time the clipboard is
+    // used (per session).
+    function copyToClipboard(text) {
+        if (window.clipboardData && window.clipboardData.setData) {
+            // IE specific code path to prevent textarea being shown while dialog is visible.
+            return clipboardData.setData("Text", text);
+
+        } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+            var textarea = document.createElement("textarea");
+            textarea.textContent = text;
+            textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+            document.body.appendChild(textarea);
+            textarea.select();
+            try {
+                return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+            } catch (ex) {
+                console.warn("Copy to clipboard failed.", ex);
+                return false;
+            } finally {
+                document.body.removeChild(textarea);
+            }
+        }
+    }
+
     $.widget('vis-ui-js.generateElements', {
         options:      {},
         declarations: {
@@ -210,13 +239,25 @@
                 }
 
                 if(has(item, 'infoText')) {
-                    var infoButton = $('<a class="infoText">Info</a>');
+                    var infoButton = $('<a class="infoText"></a>');
                     infoButton.on('click touch press',function(e){
                        var button = $(e.currentTarget);
                         $.notify(button.attr('title'),'info');
                     });
                     infoButton.attr('title', item.infoText);
                     container.append(infoButton);
+                }
+
+
+                if(has(item, 'copyClipboard')) {
+
+                    var copyButton = $('<a class="copy-to-clipboard"><i class="fa fa-clipboard" aria-hidden="true"></i></a>');
+                    copyButton.on('click', function(e) {
+                        var button = $(e.currentTarget);
+                        var data = container.formData(false);
+                        copyToClipboard(data[item.name]);
+                    });
+                    container.append(copyButton);
                 }
 
                 container.append(inputField);
