707a1480a3a658e41a87721ff5b06a72edaf559a
werrolf <rolf.neuberger@wheregroup.com>
2019-11-27 02:03:00 +0100

    Punch in attr.title for button tooltip

--- src/js/jquery.form.generator.js
+++ src/js/jquery.form.generator.js
@@ -222,7 +222,7 @@
                 var title = has(item, 'title') ? item.title : 'Submit';
                 // @todo: use .text for escaping (unless it's HTML again :\)
                 var button = $('<button>' + title + '</button>').attr(item.attr || {}).addClass('btn button');
-                button.attr("title", item.hover || title);
+                button.attr("title", (item.attr || {}).title || item.hover || title);
                 return button;
             },
             submit: function(item) {
